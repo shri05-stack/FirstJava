5b448e066a4ac7e8ff78bd27b3996e852fe51a25
Shrijan Prakash <srijanprak@gmail.com>
2022-06-13 14:51:20 +0530

    commit

--- src/studentarray.java
+++ src/studentarray.java
@@ -11,9 +11,9 @@ public class studentarray {
 		s1.gender='m';
 		s1.Roll=32;
 		s1.marks=10;
-		s2.name ="jane";
+		s2.name ="prakash";
 		s2.gender ='f';
-		s2.marks =67;
+		s2.marks =93;
 		s2.Roll =13;
 		StudentOld[] bikes= {s2,s1};
 		for(int i=0;i<bikes.length-1;i++) {
